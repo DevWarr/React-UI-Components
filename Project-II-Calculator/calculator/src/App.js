c271916363b1e21addec9fa25d8d4cbceaefb1ce
Devin Warrick <dev.warr@gmail.com>
2019-05-28 17:05:33 -0500

    Forgot to commit during project... Calculator displays properly. Working on funcitonality.

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -1,23 +1,45 @@
 import React from 'react';
 import './App.css';
+import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay.js';
+import NumberButton from './components/ButtonComponents/NumberButton.js';
+import ActionButton from './components/ButtonComponents/ActionButton.js';
 
-const App = () => {
-  return (
-    <div>
-      <h3>Welcome to React Calculator</h3>
-      <p>
-        We have given you a starter project. You'll want to build out your
-        components in their respective files, remove this code and replace it
-        with the proper components.
-      </p>
-      <p>
-        <strong>
-          Don't forget to `default export` your components and import them here
-          inside of this file in order to make them work.
-        </strong>
-      </p>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    total: "0"
+  };
+
+  render() {
+    return (
+      <div className="calculator">
+      <div className="display">
+        <CalculatorDisplay text={this.state.total}/>
+      </div>
+      <div className="buttons">
+        <div className="numbers">
+          <NumberButton buttonStyle="rectangle" text="CLEAR" />
+          <NumberButton buttonStyle="square" text="1" />
+          <NumberButton buttonStyle="square" text="2" />
+          <NumberButton buttonStyle="square" text="3" />
+          <NumberButton buttonStyle="square" text="4" />
+          <NumberButton buttonStyle="square" text="5" />
+          <NumberButton buttonStyle="square" text="6" />
+          <NumberButton buttonStyle="square" text="7" />
+          <NumberButton buttonStyle="square" text="8" />
+          <NumberButton buttonStyle="square" text="9" />
+          <NumberButton buttonStyle="rectangle" text="0" />
+        </div>
+        <div className="operators">
+          <ActionButton buttonStyle="square operator__symbol" text="÷" />
+          <ActionButton buttonStyle="square operator__symbol" text="×" />
+          <ActionButton buttonStyle="square operator__symbol" text="-" />
+          <ActionButton buttonStyle="square operator__symbol" text="+" />
+          <ActionButton buttonStyle="square operator__symbol" text="=" />
+        </div>
+      </div>
+      </div>
+    );
+  };
 };
 
 export default App;
--- Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import './Display.css';
+
+
+function CalculatorDisplay(props) {
+    return (
+        <div>
+            <h2>{props.text}</h2>
+        </div>
+    );
+};
+
+export default CalculatorDisplay;
