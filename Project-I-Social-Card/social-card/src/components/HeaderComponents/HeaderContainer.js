8cd82665c1ba60c87cdabcdf2512e6ec1c0ef557
Devin Warrick <dev.warr@gmail.com>
2019-05-27 23:11:49 -0500

    HeaderContainer html is typed out. Plan to type all html before css styling.

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,19 @@
 import React from 'react';
 import './Header.css';
+import HeaderContent from'./HeaderContent.js';
+import HeaderTitle from './HeaderTitle.js';
+import ImageThumbnail from './ImageThumbnail.js';
+
+const HeaderContainer = () => {
+    return (
+        <header>
+            <ImageThumbnail />
+            <div className="flexHeader">
+                <HeaderTitle />
+                <HeaderContent />
+            </div>
+        </header>
+    );
+};
+
+export default HeaderContainer;
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,19 @@
 import React from 'react';
 import './Header.css';
+
+
+const HeaderTitle = () => {
+    const options = {month: "short", day: "numeric"};
+    const date = new Date().toLocaleDateString("en-US", options);
+
+    return (
+        <div className="title">
+            <h3>
+                <strong>Lambda School</strong>
+                @LambdaSchool · {date}
+            </h3>
+        </div>
+    );
+};
+
+export default HeaderTitle;
